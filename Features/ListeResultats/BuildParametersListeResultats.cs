2aedb834f6d5e56ed747208cc6df9f682beb9785
gilles-dev-lab <dev@gillesmaupoint.fr>
2026-01-23 10:56:09 +0100

    Fix name constructor BuildParametersListeResultats

--- Features/ListeResultats/BuildParametersListeResultats.cs
+++ Features/ListeResultats/BuildParametersListeResultats.cs
@@ -3,7 +3,7 @@ public sealed class BuildParametersListeResultats
     public string CurrentPath { get; }
     public string SiteCode { get; }
     public string Origin { get; }
-    public BuildParameters(string origin, string currentPath, string siteCode)
+    public BuildParametersListeResultats(string origin, string currentPath, string siteCode)
     {
         CurrentPath = currentPath;
         SiteCode = siteCode;
